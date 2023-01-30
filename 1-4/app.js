d2a7781ba433dcd01360a9d452e00412dd29a15f
Ali Darvish <ali.darvish.sf@gmail.com>
2023-01-31 01:52:42 +0330

    Solve 1-4

---
+++ 1-4/app.js
@@ -0,0 +1,21 @@
+const { writeFile, unlink } = require("fs");
+
+// Creation
+
+writeFile(
+  "./third-text.txt",
+  "Created From node.js to remove with node.js!",
+  function (err) {
+    err
+      ? console.log(`Cannot create this file!`)
+      : console.log("File Created!");
+  }
+);
+
+// Removal
+
+// unlink("./third-text.txt", function (err) {
+//   err
+//     ? console.log(`There isn't any file with given path!`)
+//     : console.log(`File succesfully removed from directory!`);
+// });
